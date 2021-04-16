9748c6303930c24d1c992746d6aaeb0f81bbd26c
LirioRayce <47873309+LirioRayce@users.noreply.github.com>
2021-04-16 04:36:23 +0200

    interface

---
+++ app/view/user/pages/home.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Escola Privada</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- icon -->
+	<link rel="icon" href="" type="" />
+	<!-- <link rel="shortcut icon" href="img/ticon.ico" type="image/x-icon" /> -->
+	<meta name="description" content="Conteudo didatico, informações escolares.">
+	<meta name="author" content="Lírio Da Carlota Eduardo Rayce">
+
+
+	<!-- bootstrap -->
+	<link rel="stylesheet" type="text/css" href="../../../resorce/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../../../resorce/css/bootstrap.css">
+	<!-- Fontes css -->
+	<link rel="stylesheet" type="text/css" href="../../../resorce/fonte/css/all.css">
+	<link rel="stylesheet" type="text/css" href="../../../resorce/fonte/css/all.min.css">
+
+	<!-- meu css -->
+	<link rel="stylesheet" type="text/css" href="../../../resorce/css/meu.css">
+
+</head>
+<body>
+
+<!-- navbar-welcome -->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top bgcolor">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="#">Logo</a>
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Notícias</a>
+        </li>
+        <li class="nav-item">
+          <!-- <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> -->
+          <a class="nav-link" href="#">Contacto</a>
+        </li>
+      </ul>
+		
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a href="#" class="nav-link">Sobre</a>
+			</li>
+			<li class="nav-item">
+				<a href="#" class="nav-link">Sobre</a>
+			</li>
+		</ul>
+		
+      <!-- <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form> -->
+    </div>
+  </div>
+</nav>
+
+
+<!-- welcome-carousel -->
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+	<div class="carousel-inner">
+		<div class="carousel-item active">
+		 	<img src="../../../resorce/img/IMG (3).jpg" class="d-block w-100" alt="...">
+		 	<div class="carousel-caption">
+				<h1 class="display-2">NossaTimbila</h1>
+				<h3>Site de venda de musicas</h3>
+				<button type="button" class="btn btn-outline-light btn-lg">Inscrever-se</button>
+				<button type="button" class="btn btn-primary btn-lg">Entrar</button>
+			</div>
+		</div>
+		<div class="carousel-item">
+		 	<img src="../../../resorce/img/IMG (2).jpg" class="d-block w-100" alt="...">
+		</div>
+		<div class="carousel-item">
+		 	<img src="../../../resorce/img/IMG (1).jpg" class="d-block w-100" alt="...">
+		</div>
+	</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Anterior</span>
+		</button>
+
+		<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Proxima</span>
+		</button>
+</div>
+
+<!-- alert with close butten -->
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+ Desejamos voltar ao normal, mas <strong>O normal levou nos a isso</strong>. Para evitar as futuras pandemias que sabemos que estão chegando, DEVEMOS lutar contra todas as formas como a normalidade nos falhou. Temos que construir algo melhor.
+ <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
+
+<!-- welcome container -->
+<div class="container-fluid padding">
+	<div class="row welcome text-center">
+		<div class="col-12">
+			<h1 class="display-4">Seja Bem Vindo</h1>
+		</div>
+		<hr>
+		<div class="col-12">
+			<p class="lead">Aqui encontrará todas as informações que busca ter a cerca da escola.
+		</div>
+	</div>
+	<hr>
+</div>
+<br>
+
+<div class="col-12 container-fluid">
+	<h3>Notícias</h3>
+</div>
+<!-- conteudos -->
+<div class="container-fluid">
+	<div class="row padding">
+		<div class="col-md-3">
+			<div class="card" style="width: 18rem;">
+				<img src="../../../resorce/img/image 4.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content
+							<br>
+						<a href="#" class="btn btn-primary">Ver mais</a>
+					</div>
+			</div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="card" style="width: 18rem;">
+				<img src="../../../resorce/img/image 4.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content
+							<br>
+						<a href="#" class="btn btn-primary">Ver mais</a>
+					</div>
+			</div>
+		</div>
+
+		<div class="col-md-3">
+			<div class="card" style="width: 18rem;">
+				<img src="../../../resorce/img/image 4.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content
+							<br>
+						<a href="#" class="btn btn-primary">Ver mais</a>
+					</div>
+			</div>
+		</div>
+
+
+		<div class="col-md-3">
+			<div class="card" style="width: 18rem;">
+				<img src="../../../resorce/img/image 4.jpg" class="card-img-top" alt="...">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content
+							<br>
+						<a href="#" class="btn btn-primary">Ver mais</a>
+					</div>
+			</div>
+		</div>
+
+	</div>
+</div>
+
+
+
+
+
+
+<footer class="footer">
+	
+</footer>
+
+
+
+
+	<!-- swiper js -->
+  	<script type="text/javascript" src="../../../resorce/swiper.min.css"></script>
+  	<!-- js bootstrap -->
+	<script type="text/javascript" src="../../../resorce/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../../../resorce/js/bootstrap.js"></script>
+	<!-- jquery -->
+	<script type="text/javascript" src="../../../resorce/js/jquery-3.6.0.min.js"></script>
+	<script type="text/javascript" src="../../../resorce/js/jquery-3.6.0.slim.min.js"></script>
+	<!-- fontes js -->
+	<script type="text/javascript" src="../../../resorce/fonte/js/all.js"></script>
+	<script type="text/javascript" src="../../../resorce/fonte/js/all.min.js"></script>
+
+
+	<script>
+		$(window).scroll(function(){
+			$('nav').toggleClass('scrolled',$(this).scrollTop()>50);
+		});
+	</script>
+	
+</body>
+</html>
